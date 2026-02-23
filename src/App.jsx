8c707f4388fd0f9c1efca2c3eab0dad50f5f9924
laurabrilhantee <laurabrilhante8@gmail.com>
2026-02-23 08:03:12 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 
-export default function App() {
+function App() {
   return (
     <>
       <div className="auth-container">
@@ -10,8 +10,73 @@ export default function App() {
           <div className="gradient-orb orb-2"></div>
           <div className="gradient-orb orb-3"></div>
         </div>
+
+        <div className="auth-card">
+          <div className="auth-header">
+            <h1>Bem-Vindo</h1>
+            <p>Entre para continuar</p>
+          </div>
+
+          <form className='auth-form'>
+          <div className="form-group">
+            <label htmlFor="usuario">Usuário</label>
+            <input 
+              type="text" 
+              id='usuario' 
+              name='usuario' 
+              placeholder='Escolha seu nome de usuário' 
+              required  />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="email">Usuário ou Email</label>
+            <input 
+              type="text" 
+              id='email' 
+              name='email' 
+              placeholder='Digite seu usuário ou email' 
+              required  />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="senha">Senha</label>
+            <input 
+              type="password" 
+              id='senha' 
+              name='senha' 
+              placeholder='*********' 
+              required  />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="confirmarSenha">Confirmar Senha</label>
+            <input 
+              type="password" 
+              id='confirmarSenha' 
+              name='confirmarSenha' 
+              placeholder='*********' 
+              required  />
+          </div>
+
+          <div className="forgot-password">
+            <a href="#">Esqueceu a senha?</a>
+          </div>
+
+          <button type='submit' className='submit-btn'>
+            Entrar
+          </button>
+          </form>
+
+          <div className="auth-footer">
+            <p>Não tem uma conta?</p>
+            <button className='toggle-btn'>Cadastre-se</button>
+          </div>
+
+        </div>
+
       </div>
     </>
   )
 }
 
+export default App
